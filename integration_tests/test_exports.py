6ddf1c3ba6cc94114b1a389bfb83da7b07f4f2e9
Jacob Williams <jacobaw@gmail.com>
2020-04-10 00:24:30 -0700

    switch to pytest

--- integration_tests/test_exports.py
+++ integration_tests/test_exports.py
@@ -3,40 +3,35 @@ import contextlib
 import csv
 import json
 import io
-import unittest
 from exporteer_todoist import cli
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 
-class ExportsTestCase(unittest.TestCase):
-    def test_full_sync(self):
-        out = io.StringIO()
-        with contextlib.redirect_stdout(out):
-            cli.full_sync(None)
-        result = out.getvalue()
-        parsed = json.loads(result)
-        keys = parsed.keys()
-        self.assertIn('projects', keys)
-        self.assertIn('items', keys)
-        self.assertIn('notes', keys)
-        self.assertGreater(len(parsed['projects']), 1)
-        self.assertGreater(len(parsed['items']), 1)
-
-    def test_latest_backup(self):
-        with TemporaryDirectory() as rawpath:
-            args = argparse.Namespace()
-            args.target_dir = [rawpath]
-            cli.latest_backup(args)
-            path = Path(rawpath)
-            cpaths = list(path.glob('*.csv'))
-            self.assertGreater(len(cpaths), 1)
-            maxlen = 0
-            for cpath in cpaths:
-                with open(cpath) as file:
-                    maxlen = max(maxlen, len(list(csv.reader(file))))
-            self.assertGreater(maxlen, 1)
+def test_full_sync():
+    out = io.StringIO()
+    with contextlib.redirect_stdout(out):
+        cli.full_sync(None)
+    result = out.getvalue()
+    parsed = json.loads(result)
+    keys = parsed.keys()
+    assert 'projects' in keys
+    assert 'items' in keys
+    assert 'notes' in keys
+    assert len(parsed['projects']) > 1
+    assert len(parsed['items']) > 1
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_latest_backup():
+    with TemporaryDirectory() as rawpath:
+        args = argparse.Namespace()
+        args.target_dir = [rawpath]
+        cli.latest_backup(args)
+        path = Path(rawpath)
+        cpaths = list(path.glob('*.csv'))
+        assert len(cpaths) > 1
+        maxlen = 0
+        for cpath in cpaths:
+            with open(cpath) as file:
+                maxlen = max(maxlen, len(list(csv.reader(file))))
+        assert maxlen > 1
