acc716dbc55ed3084428a6a167f0820d5bdb2093
Jacob Williams <jacobaw@gmail.com>
2020-04-10 14:52:03 -0700

    doc

--- src/exporteer_todoist/cli.py
+++ src/exporteer_todoist/cli.py
@@ -1,3 +1,6 @@
+"""Command-line interface for exporteer_todoist tool."""
+
+
 import argparse
 import os
 import requests
@@ -5,7 +8,7 @@ import sys
 from operator import itemgetter
 
 
-def get_token():
+def _get_token():
     token = os.environ.get('TODOIST_API_TOKEN', None)
     if not token:
         print('TODOIST_API_TOKEN must be set', file=sys.stderr)
@@ -13,8 +16,8 @@ def get_token():
     return token
 
 
-def full_sync(args):
-    token = get_token()
+def _full_sync(args):
+    token = _get_token()
     params = {'token': token, 'sync_token': '*', 'resource_types': '["all"]'}
     resp = requests.get('https://api.todoist.com/sync/v8/sync', params=params)
     resp.raise_for_status()
@@ -22,8 +25,8 @@ def full_sync(args):
     return 0
 
 
-def latest_backup(args):
-    token = get_token()
+def _latest_backup(args):
+    token = _get_token()
     params = {'token': token}
     resp = requests.get('https://api.todoist.com/sync/v8/backups/get',
                         params=params)
@@ -45,6 +48,11 @@ def latest_backup(args):
 
 
 def main(args=None):
+    """Runs the tool and returns its exit code.
+
+    args may be an array of string command-line arguments; if absent,
+    the process's arguments are used.
+    """
     parser = argparse.ArgumentParser()
     parser.set_defaults(func=None)
 
@@ -52,11 +60,11 @@ def main(args=None):
 
     p_full_sync = subs.add_parser('full_sync', help='export the json output' +
                                                     ' of the sync endpoint')
-    p_full_sync.set_defaults(func=full_sync)
+    p_full_sync.set_defaults(func=_full_sync)
 
     p_latest_backup = subs.add_parser('latest_backup',
                                       help='download latest backup zip')
-    p_latest_backup.set_defaults(func=latest_backup)
+    p_latest_backup.set_defaults(func=_latest_backup)
 
     args = parser.parse_args(args)
     if not args.func:
