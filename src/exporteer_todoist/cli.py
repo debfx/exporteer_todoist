d177f974f12aa594a625c75de8f7eaee0c5f9670
Felix Geyer <debfx@fobos.de>
2024-06-10 22:19:45 +0200

    Add support for downloading attachments

--- src/exporteer_todoist/cli.py
+++ src/exporteer_todoist/cli.py
@@ -2,10 +2,21 @@
 
 
 import argparse
+import csv
+import io
+import json
 import os
+import re
 import requests
 import sys
+import typing
+import zipfile
 from operator import itemgetter
+
+
+class AttachmentInfo(typing.NamedTuple):
+    name: str
+    url: str
 
 
 def _get_token():
@@ -26,6 +37,40 @@ def _full_sync(args):
     return 0
 
 
+def _extract_attachments(csv_bytes: bytes) -> list[AttachmentInfo]:
+    attachments = []
+
+    csv_reader = csv.DictReader(csv_bytes.decode().split('\n'))
+    for row in csv_reader:
+        for match in re.finditer(r'\[\[file\s+({.*?})\]\]', row['CONTENT']):
+            attachment_data = json.loads(match.group(1))
+            attachment_info = AttachmentInfo(name=attachment_data['file_name'], url=attachment_data['file_url'])
+            if attachment_info.url.startswith('https://files.todoist.com/'):
+                attachments.append(attachment_info)
+
+    return attachments
+
+
+def _backup_attachments(backup_file, download_headers) -> None:
+    backup_file.seek(0)
+    zip_file = zipfile.ZipFile(backup_file, mode='a')
+
+    for path in zip_file.namelist():
+        if not path.endswith('.csv'):
+            continue
+
+        # strip .csv file extension
+        project_name = path[:-4]
+
+        with zip_file.open(path, mode='r') as file_csv:
+            attachments = _extract_attachments(file_csv.read())
+
+        for attachment in attachments:
+            resp = requests.get(attachment.url, headers=download_headers)
+            with zip_file.open(f'attachments/{project_name}/{attachment.name}', mode='w') as file_attachment:
+                file_attachment.write(resp.content)
+
+
 def _latest_backup(args):
     token = _get_token()
     headers = {'Authorization': f'Bearer {token}'}
@@ -39,15 +84,21 @@ def _latest_backup(args):
         return 2
 
     if args.output == '-':
-        output_file = sys.stdout.buffer
+        output_file = io.BytesIO()
     else:
-        output_file = open(args.output, 'wb')
+        output_file = open(args.output, 'w+b')
 
     url = versions[0]['url']
     resp = requests.get(url, headers=headers)
     resp.raise_for_status()
     output_file.write(resp.content)
-    output_file.flush()
+
+    if args.attachments:
+        _backup_attachments(output_file, headers)
+
+    if args.output == '-':
+        sys.stdout.buffer.write(output_file.getbuffer())
+        sys.stdout.buffer.flush()
 
     return 0
 
@@ -71,6 +122,8 @@ def main(args=None):
                                       help='download latest backup zip')
     p_latest_backup.add_argument('--output', '-o', default='-',
                                  help='path to write the zip file to (defaults to "-" for stdout)')
+    p_latest_backup.add_argument('--attachments', action='store_true',
+                                 help='Download attachments in addition to the task CSVs')
     p_latest_backup.set_defaults(func=_latest_backup)
 
     args = parser.parse_args(args)
